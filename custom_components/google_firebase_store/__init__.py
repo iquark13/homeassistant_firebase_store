1e37ee7a53019ecfc6ac6f83291e7d2e20e30cc0
Iquark13 <iquark13@quarkys.com>
2021-01-03 00:30:00 -0800

    Working through script to define how it works in comments

--- custom_components/google_firebase_store/__init__.py
+++ custom_components/google_firebase_store/__init__.py
@@ -41,29 +41,52 @@ url = 'http://localhost:8123/api/services/homeassistant/turn_on'
 
 def setup(hass: HomeAssistant, yaml_config: Dict[str, Any]):
     """Activate Google Firebase component."""
-
-    config = yaml_config[DOMAIN]
+    
+    #The hass.config is passed into setup as the yaml_config - this pulls the settings from the component
+        #like the filter values, or the token values, or the firebase auth file location.
+    config = yaml_config[DOMAIN] 
+    
+    #This simply pulls together the path to the firebase private key file.
     service_principal_path = os.path.join(
         hass.config.config_dir, config[CONF_SERVICE_PRINCIPAL]
     )
     
+    #Use the token that we provided in the !secret file -> store the request format in hed
     token = config[CONF_WEB_TOKEN]
     hed = {'Authorization': 'Bearer ' + token}
 
+    #Check and make sure that the credentials exist.
     if not os.path.isfile(service_principal_path):
         _LOGGER.error("Path to credentials file cannot be found")
         return False
 
+    #This uses the config[DOMAIN] from the hass.config passed to setup and the global filter name to
+        #create a filter list of entity types. THe filter domains are passed as a python list from the config.yaml
     entities_filter = config[CONF_FILTER]
 
+    #Use firebase credentials to store the firebase credentials into the cred variable
     cred = credentials.Certificate(service_principal_path)
+    
+    #Use the credentials to setup access to the firebase object
     default_app = firebase_admin.initialize_app(cred)
+
+    #Setup a firestore (database/storage items) in the db variable.
     db = firestore.client()
 
 
 
     def send_to_pubsub(event: Event):
-        """Send states to Firebase """
+        '''Send states to Firebase
+        
+            Inputs: Receives an event from the hass.bus.listener
+
+            Actions: Checks if state exists, is unknown/unavailable (useless statusses), or if it isn't in the domain from the filter
+
+            If: it isn't for our stuff, then jump out.
+
+            Else: store it in the database on firestore.
+        
+        '''
         state = event.data.get("new_state")
         if (
             state is None
@@ -71,12 +94,15 @@ def setup(hass: HomeAssistant, yaml_config: Dict[str, Any]):
             or not entities_filter(state.entity_id)
         ):
             return
-
+        
+        #Goes into the collection (top level) and grabs a specific database entry (document)
+            #Note - the 'u' before the collection name signifies that this is a unicode string
         doc_ref = db.collection(u'homeassistant').document(state.entity_id)
 
+        #Sets the previously grabbed document (database entry) to the state of the item that fired the event.
         doc_ref.set(state.as_dict())
 
-    hass.bus.listen(EVENT_STATE_CHANGED, send_to_pubsub)
+    hass.bus.listen(EVENT_STATE_CHANGED, send_to_pubsub) #This seems like it is calling the private bus component of the class maybe change to eventbus?
     
     def fire_event(col_snapshot, changes, read_time):
         print(u'Callback received query snapshot.')
